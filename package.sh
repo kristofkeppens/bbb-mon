1fe143491e7b1581aaa14502837b0cff604e0136
Kristof Keppens <kkeppens@gmail.com>
2013-01-18 13:52:52 +0100

    fix permissions on folder before building package

--- package.sh
+++ package.sh
@@ -1,9 +1,13 @@
 NAME=bbb-mon
-VERSION=1.0
+VERSION=1.0.1
+
+sudo mkdir /usr/local/bbb-mon
+sudo cp bigbluebutton_info.py bbb_api.py get-bigbluebutton-info.py /usr/local/bbb-mon
+sudo chmod 755 /usr/local/bbb-mon/
+sudo chown -R root:root /usr/local/bbb-mon
 
 fpm -s dir -t deb -n ${NAME} -v ${VERSION} \
     --description "bigbluebutton monitoring scripts" \
     -d python \
     -d python-argparse \
-    --prefix /usr/local/bbb-mon \
-    bigbluebutton_info.py bbb_api.py get-bigbluebutton-info.py
+    /usr/local/bbb-mon 
